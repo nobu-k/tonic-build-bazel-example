a68c89f4cf12d00488d26dfa085c965f4da40e53
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2024-07-21 12:54:43 -0700

    feat: support build using Bazel

--- protos/hello/build.rs
+++ protos/hello/build.rs
@@ -1,6 +1,21 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
+    #[cfg(feature = "bazel")]
+    {
+        let r = runfiles::Runfiles::create()?;
+        let protoc_loc = std::env::var("PROTOC_RLOCATION").expect("PROTOC_RLOCATION must be set");
+        let protoc_path = r.rlocation(protoc_loc.as_str());
+        let protoc_dir = protoc_path.parent().unwrap();
+        std::env::set_var(
+            "PATH",
+            format!(
+                "{}:{}",
+                protoc_dir.to_str().unwrap(),
+                std::env::var("PATH").unwrap(),
+            ),
+        );
+    }
     // TODO: add file_descriptor_set_path
     tonic_build::configure()
         .build_client(true)
